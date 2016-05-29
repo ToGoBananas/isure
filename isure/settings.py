4944bc8ae99ad572510dd57f8cb2a74e2c495dc6
Andrei Khaliaukin <endpoo@gmail.com>
2016-05-29 13:09:00 +0300

    settings

--- isure/settings.py
+++ isure/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'isure',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'isure',
+        'PASSWORD': 'isure',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },

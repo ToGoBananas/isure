1ccc989fdbf7bcf5f9b0c3ecac5b80be450bb98b
Andrei Khaliaukin <endpoo@gmail.com>
2016-05-29 13:11:12 +0300

    restore missing code

--- configuration/signals.py
+++ configuration/signals.py
@@ -3,10 +3,10 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.core.files.base import ContentFile
 
-# from .helpers import get_bordereau
-#
-#
-# @receiver(pre_save, sender=Bordereau)
-# def create_file(sender, instance=None, **kwargs):
-#     if not instance.csv:
-#         instance.csv.save('test.csv', ContentFile(get_bordereau(instance.start, instance.end)))
+from .helpers import get_bordereau
+
+
+@receiver(pre_save, sender=Bordereau)
+def create_file(sender, instance=None, **kwargs):
+    if not instance.csv:
+        instance.csv.save('test.csv', ContentFile(get_bordereau(instance.start, instance.end)))

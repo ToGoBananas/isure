d31ee4c35d1eb9465bac39294b8c9938087648da
Andrei Khaliaukin <endpoo@gmail.com>
2016-05-31 17:12:00 +0300

    migration fix

--- configuration/signals.py
+++ configuration/signals.py
@@ -1,12 +1,12 @@
-from .models import Bordereau
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.core.files.base import ContentFile
-
-from .helpers import get_bordereau
-
-
-@receiver(pre_save, sender=Bordereau)
-def create_file(sender, instance=None, **kwargs):
-    if not instance.csv:
-        instance.csv.save('test.csv', ContentFile(get_bordereau(instance.start, instance.end)))
+# from .models import Bordereau
+# from django.db.models.signals import pre_save
+# from django.dispatch import receiver
+# from django.core.files.base import ContentFile
+#
+# from .helpers import get_bordereau
+#
+#
+# @receiver(pre_save, sender=Bordereau)
+# def create_file(sender, instance=None, **kwargs):
+#     if not instance.csv:
+#         instance.csv.save('test.csv', ContentFile(get_bordereau(instance.start, instance.end)))
